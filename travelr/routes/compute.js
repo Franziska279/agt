4e9bd44128f3e79bdae7e4b881ca1afed9ac78f4
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2021-12-30 19:46:07 +0100

    Added modify placeholder content and compute page with simple access restriction

--- travelr/routes/compute.js
+++ travelr/routes/compute.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET something to list maybe?. */
 router.get('/', handleRedirect);
 router.post('/', handleRedirect);
 
 function handleRedirect(req, res, next) {
-    if (req.body.fromIndex != null) {
-        res.render('modify', { title: 'Travelr' });
+    if (req.body.fromModify != null) {
+        res.render('compute', { title: 'Travelr' });
     } else {
         res.redirect('../')
     }
