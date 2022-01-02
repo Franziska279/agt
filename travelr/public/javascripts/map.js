f3f7d7ada0f9f3e75b3ef8d08640b575f4bbd356
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2022-01-02 18:28:36 +0100

    Added pins for map

--- travelr/public/javascripts/map.js
+++ travelr/public/javascripts/map.js
@@ -14,4 +14,20 @@ document.addEventListener("DOMContentLoaded", function() {
             zoom: 8,
             center: { lat: 38.72807471947513, lng: 16.539936297329433 }
         });
+
+    // TODO: test data
+    var coordinates = [[39.298403014978675, 16.258142827309133],
+        [38.67694273628852, 15.895550990399894],
+        [38.09394081111101, 15.642539653630289],
+        [38.91023583099221, 16.579782783050636]];
+    coordinates.forEach(c => {
+        // Create an icon, an object holding the latitude and longitude, and a marker:
+        let icon = new H.map.Icon("images/marker_map_icon.png", {size: {w: 30, h: 30}}),
+            coords = {lat: c[0], lng: c[1]},
+            marker = new H.map.Marker(coords, {icon: icon});
+
+        // Add the marker to the map and center the map at the location of the marker:
+        map.addObject(marker);
+        //map.setCenter(coords);
+    });
 });
