d4f7db079eebbb96ad883d312c99f4e6c016c050
KerstinGreifensteiner <kerstin.greifensteiner@gmx.at>
2022-01-04 16:59:06 +0100

    Added dynamic routing by finding coordinates and dynamic result display

--- travelr/public/javascripts/map.js
+++ travelr/public/javascripts/map.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+async function set_map(cities) {
     // TODO: divide into functions! (Refactoring)
 
     // obtain data from form
@@ -10,6 +11,33 @@ document.addEventListener("DOMContentLoaded", function() {
         'apikey': 'IbLuSWOk8u4G011cI7N8QM1vIZXHHGbBNBwbZ8VkXpc'
     });
 
+    // TODO: add start and end dynamically
+    var coordinates = [[39.298403014978675, 16.258142827309133]];
+    // TODO: geocode service
+    var service = platform.getSearchService();
+    for (const c of cities) {
+        let name = c.substring(0, c.lastIndexOf(" "));
+        let postalCode = c.substring(c.lastIndexOf(" "));
+        await service.geocode({
+            q: postalCode + ", " + name
+        }, (result) => {
+            // Add a marker for each location found
+            result.items.forEach((item) => {
+                //map.addObject(new H.map.Marker(item.position));
+                coordinates.push([item.position.lat, item.position.lng]);
+            });
+        }, alert);
+
+    }
+    coordinates.push([39.298403014978675, 16.258142827309133]);
+
+    // TODO: test data
+    // var coordinates = [[39.298403014978675, 16.258142827309133],
+    //     [38.67694273628852, 15.895550990399894],
+    //     [38.09394081111101, 15.642539653630289],
+    //     [38.91023583099221, 16.579782783050636]];
+
+    // TODO: map
     // Obtain the default map types from the platform object:
     var defaultLayers = platform.createDefaultLayers();
 
@@ -29,11 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-    // TODO: test data
-    var coordinates = [[39.298403014978675, 16.258142827309133],
-        [38.67694273628852, 15.895550990399894],
-        [38.09394081111101, 15.642539653630289],
-        [38.91023583099221, 16.579782783050636]];
+    console.log(coordinates);
     coordinates.forEach(c => {
         // Create an icon, an object holding the latitude and longitude, and a marker:
         let icon = new H.map.Icon("images/marker_map_icon.png", {size: {w: 30, h: 30}}),
@@ -57,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function() {
         'transportMode': 'car',
         // The start point of the route:
         'origin': start,
-        'via': new H.service.Url.MultiValueQueryParameter( waypoints ),
+        'via': new H.service.Url.MultiValueQueryParameter(waypoints),
         // The end point of the route:
         'destination': start,
         // Include the route shape in the response
@@ -66,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     // Define a callback function to process the routing response:
-    var onResult = function(result) {
+    var onResult = function (result) {
         // ensure that at least one route was found
         if (result.routes.length) {
             result.routes[0].sections.forEach((section) => {
@@ -95,7 +119,8 @@ document.addEventListener("DOMContentLoaded", function() {
                             strokeColor: 'rgba(255, 255, 255, 1)',
                             lineDash: [0, 2],
                             lineTailCap: 'arrow-tail',
-                            lineHeadCap: 'arrow-head' }
+                            lineHeadCap: 'arrow-head'
+                        }
                     }
                 );
                 // create a group that represents the route line and contains
@@ -115,7 +140,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 // TODO: set map viewpoint to whole route
                 // Set the map's viewport to make the whole route visible:
                 // map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
-            //console.log(result.routes[0])
+                //console.log(result.routes[0])
                 // TODO: calculate distance by getting distances between each waypoint
             });
         }
@@ -128,7 +153,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // the callback and an error callback function (called if a
     // communication error occurs):
     router.calculateRoute(routingParameters, onResult,
-        function(error) {
+        function (error) {
             alert(error.message);
         });
-});
+}
--- travelr/public/javascripts/processData.js
+++ travelr/public/javascripts/processData.js
@@ -1,6 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     let data = document.getElementById("result-data").innerHTML;
     let dataJson = JSON.parse(data);
-    console.log(dataJson);
+    // console.log(dataJson);
+    let participants = [];
+    let cities = new Set();
+    dataJson.forEach(d => {
+        participants.push(d["name"]);
+        for(let p in d["preferences"]) {
+            cities.add(p);
+        }
+    });
+    // console.log(participants);
+    // console.log(cities);
+    // TODO: calculate result here!
+    let resultDiv = document.getElementById("result-participant-div");
+    // TODO: costs for each participant is missing
+    participants.forEach(p => {
+        let pEl = document.createElement("p");
+        pEl.innerHTML = p + ': 200€';
+        resultDiv.appendChild(pEl);
+    });
 
+    let tourDiv = document.getElementById("route-text-div");
+    let tourPElem = tourDiv.getElementsByTagName("p")[0];
+    tourPElem.innerHTML = "Cosenza";
+    cities.forEach(c => {
+        // TODO: also send starting city
+        tourPElem.innerHTML += " - " + c.substring(0, c.lastIndexOf(" "));
+    });
+    tourPElem.innerHTML += " - Cosenza";
+    set_map(cities);
 });
