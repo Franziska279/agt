eceda9d000b95e600dab067ccdb47e4b362b68f9
Franziska279 <franzi27mueller@gmail.com>
2022-01-12 15:58:44 +0100

    calculate kcombinations - rebase

--- travelr/public/javascripts/calculateResult.js
+++ travelr/public/javascripts/calculateResult.js
@@ -8,7 +8,21 @@ async function getResult(json) {
     let cityCost = json["city_cost"];
     let start = json["start"];
     let participantsData = json["data"];
+    console.log(json)
+
+    // let cities = new Set();
+    // participantsData.forEach(d => {
+    //     for(let p in d["preferences"]) {
+    //         cities.add(p); // city = Cosenza;87100
+    //     }
+    // });
     let cities = json["cities"]
+
+    let participants = new Set();
+    participantsData.forEach(d => {
+        participants.add(d["name"]); // participant = Franzi;
+
+    });
 
     // TODO: IMPLEMENT ALGORITHM HERE!
 
@@ -24,5 +38,48 @@ async function getResult(json) {
     // }
 
     // console.log(resultJson);
+    // Compute Combinations
+    participants_combination = new Set(getCombinations(Array.from(participants), k));
+    cities_combination = new Set(getCombinations(Array.from(cities), cities.size));
+
+    // Check with logging
+    console.log(participants)
+    console.log(cities)
+    console.log(participants_combination)
+    console.log(cities_combination)
+
+    // First For-Loop
+
+
+    console.log(resultJson);
     return resultJson;
+}
+
+
+// Function to generate all combinations of cities or participants
+function getCombinations(valuesArray, k)
+{
+    var combi = [];
+    var temp = [];
+    var slent = Math.pow(2, valuesArray.length);
+
+    for (var i = 0; i < slent; i++)
+    {
+        temp = [];
+        for (var j = 0; j < valuesArray.length; j++)
+        {
+            if ((i & Math.pow(2, j)))
+            {
+                temp.push(valuesArray[j]);
+            }
+        }
+        if (temp.length > 0 &&  temp.length <= k )
+        {
+            combi.push(temp);
+        }
+    }
+
+    combi.sort((a, b) => a.length - b.length);
+    //console.log(combi.join("\n"));
+    return combi;
 }
